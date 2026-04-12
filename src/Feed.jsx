f1ce9c0af9ff61ce01c6400297bfd5ba9303cd60
IBandbyte <c-kleanthous@hotmail.co.uk>
2026-04-12 03:00:02 +0100

    fix(feed): restore right rail lower visibility
    - Reduced right rail bottom offset from 126px to 32px - Restores vertical space for lower action icons - Fixes bottom clipping on the right rail

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1206,7 +1206,7 @@ topLiveFixedButton: {
     position: "absolute",
     right: 14,
     top: "138px",
-    bottom: "126px",
+    bottom: "32px",
     zIndex: 9,
     width: 74,
     display: "flex",
