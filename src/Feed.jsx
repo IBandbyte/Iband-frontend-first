900167863cd4b872c0d591e8c6862360f776f6b1
IBandbyte <c-kleanthous@hotmail.co.uk>
2026-06-01 01:16:22 +0100

    refactor(feed): tune rail counter spacing using numberTopGap
    - use share icon as visual spacing baseline - increase like counter clearance - increase boost counter clearance - slightly increase info counter clearance - preserve fixed icon box architecture - preserve number box and spacer box system - continue right rail harmony pass

--- src/Feed.jsx
+++ src/Feed.jsx
@@ -1222,7 +1222,7 @@ boxShadow:
   value={displayLikes}
   label="Likes"
   scale={actionScale}
-  numberTopGap={6}
+  numberTopGap={8}
 >
   <RightRailIconImage
     src={isActiveLiked ? RIGHT_RAIL_ICONS.likeRed : RIGHT_RAIL_ICONS.likeWhite}
@@ -1291,7 +1291,7 @@ boxShadow:
   value={displayBoosts}
   label="Boosts"
   scale={actionScale}
-  numberTopGap={8}
+  numberTopGap={12}
 >
   <RightRailIconImage
     src={isActiveBoosted ? RIGHT_RAIL_ICONS.boostGold : RIGHT_RAIL_ICONS.boostWhite}
